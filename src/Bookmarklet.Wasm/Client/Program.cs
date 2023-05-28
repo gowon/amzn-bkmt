459eae62f8afe787d263098d2d232f9f39e9f64f
Gowon Patterson <contact@gowon.dev>
2023-05-28 15:58:37 -0400

    Scaffolded form tool.
    Removed unnecessary boilerplate.

--- src/Bookmarklet.Wasm/Client/Program.cs
+++ src/Bookmarklet.Wasm/Client/Program.cs
@@ -1,20 +1,19 @@
-using Bookmarklet.Wasm;
+namespace Bookmarklet.Wasm;
+
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
-namespace Bookmarklet.Wasm
+public class Program
 {
-    public class Program
+    public static async Task Main(string[] args)
     {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
-            builder.RootComponents.Add<HeadOutlet>("head::after");
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+        builder.RootComponents.Add<App>("#app");
+        builder.RootComponents.Add<HeadOutlet>("head::after");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-            await builder.Build().RunAsync();
-        }
+        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+        builder.Services.AddMudServices();
+        await builder.Build().RunAsync();
     }
 }
